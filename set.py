14dd1ea6ae836814156e9f968129384878d1523a
tasa1998 <58151458+tasa1998@users.noreply.github.com>
2020-02-22 15:37:14 +0100

    #parsiranje_skupa_HTML_dokumenata

---
+++ set.py
@@ -0,0 +1,32 @@
+import copy
+class Set:
+    def __init__(self):
+        self.reci={}
+
+    def dodaj(self, link):
+        if link not in self.reci:
+            self.reci[link]=link
+
+    def obrisi(self, link):
+        del (self.reci[link])
+
+    def unija(self, skup):
+        unijaa=copy.copy(self)
+        for i in skup.reci:
+            if i not in self.reci:
+                unijaa.dodaj(i)
+        return unijaa
+
+    def presek(self, skup):
+        presekk=Set()
+        for i in self.reci:
+            if self[i] == skup[i]:
+                presekk.dodaj(i)
+        return presekk
+
+    def komplement(self, skup):
+        komplementt = copy.copy(self)
+        for i in skup.reci:
+            if i in self.reci:
+                komplementt.obrisi(i)
+        return komplementt
