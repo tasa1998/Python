38e46e69d45575ef60e950a49f9e5805b2e9a60f
tasa1998 <58151458+tasa1998@users.noreply.github.com>
2020-02-25 01:07:46 +0100

    #parsiranje_skupa_HTML_dokumenata

---
+++ src/structure/vertex.py
@@ -0,0 +1,21 @@
+from src.text.parser import Parser
+class Vertex:
+    """Lightweight vertex structure for a graph."""
+    def __init__(self, ime, linkovi, reci):
+        self.ime = ime
+        self.linkovi = linkovi
+        self.reci = reci
+        self.veze = set() #skup cvorova koji pokazuju na ovaj cvor
+
+    """def element(self):
+        Return element associated with this vertex.
+        return self._element"""
+
+    def parsiraj(self):
+        parserr = Parser()
+        return parserr.parse(self.ime)
+
+    """def __hash__(self): # will allow vertex to be a map/set key
+        return hash(id(self))
+    def __str__(self):
+        return str(self._element)"""
